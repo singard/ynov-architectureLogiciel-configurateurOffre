654f4464e806b45d3a16a94dff9f0408efe7bbdd
singarde <mathieugonza3@gmail.com>
2021-12-03 09:13:48 +0100

    commit initial pour l'api produit

---
+++ apiProduit/src/main/java/com/ynov/architectureLogiciel/apiProduit/ApiProduitApplication.java
@@ -0,0 +1,13 @@
+package com.ynov.architectureLogiciel.apiProduit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiProduitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiProduitApplication.class, args);
+	}
+
+}
---
+++ apiProduit/src/test/java/com/ynov/architectureLogiciel/apiProduit/ApiProduitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ynov.architectureLogiciel.apiProduit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiProduitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
